6bf4fd09460faf908f83d8d03812d66b1846e53a
Kyle Vedder <kyle.c.vedder@gmail.com>
2019-12-10 22:14:50 -0500

    Updated web to use names.

--- control_stack/scripts/node/web_goto_node.py
+++ control_stack/scripts/node/web_goto_node.py
@@ -6,22 +6,16 @@ import SocketServer
 import socket
 import signal
 from subprocess import check_output
-out = check_output(["ifconfig"])
 
-def get_ip(out):
-    devices = out.split("\n\n")
-    for d in devices:
-        if "inet 10." in d:
-            return d.split("inet ")[1].split(" netmask")[0].strip()
-    raise ValueException
 
 PORT = 8000
 pub = rospy.Publisher('/nav_goal', Pose2D, queue_size=10)
 rospy.init_node('goal_publisher', anonymous=True)
 rate = rospy.Rate(100) # 10hz
 
-shutting_down = False
+named_locations = {"door" : [-1, -1, 0], "water": [-2, -7, 0], "middle": [-1.5, -3, 0]}
 
+shutting_down = False
 def signal_handler(signal, frame):
     print('Killing Server!')
     global shutting_down
@@ -40,24 +34,43 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.send_header("Content-type", "text/plain")
         self.end_headers()
 
+    def _set_headers_fail(self):
+        self.send_response(400)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+
+    def _get_position_from_name(self, name):
+        try:
+            return named_locations[name.strip().lower()]
+        except:
+            return None
+
     def do_POST(self):
         content_len = int(self.headers.getheader('content-length', 0))
         post_body = self.rfile.read(content_len)
-        arr = post_body.split(' ')
-        if len(arr) != 3:
+        position = self._get_position_from_name(post_body)
+        if position is None:
+            self._set_headers_fail()
+            self.wfile.write("Unknown location: {}".format(post_body))
             return
-        arr = [float(e) for e in arr]
-        print(arr)
         for _ in range(10):
-            pub.publish(*arr)
+            pub.publish(*position)
         self._set_headers()
-        self.wfile.write("Set goal to {} {} {}".format(*arr))
+        self.wfile.write("Set goal to {} {} {} ({})".format(position[0], position[1], position[2], post_body))
+
+def get_ip():
+    out = check_output(["ifconfig"])
+    devices = out.split("\n\n")
+    for d in devices:
+        if "inet 10." in d:
+            return d.split("inet ")[1].split(" netmask")[0].strip()
+    raise ValueException
 
 Handler = ServerHandler
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 httpd.allow_reuse_address = True
-ip = get_ip(out)
+ip = get_ip()
 print("Server IP Address: {}:{}".format(ip, PORT))
 try:
     httpd.serve_forever()
