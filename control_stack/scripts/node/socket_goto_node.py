63bf231ac4d92fd254fe8924fef3de234dce3b4c
Kyle Vedder <kyle.c.vedder@gmail.com>
2019-12-15 15:03:27 -0500

    Added proper socket comms support.

--- control_stack/scripts/node/socket_goto_node.py
+++ control_stack/scripts/node/socket_goto_node.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import socket
 import sys
+import rospy
+from geometry_msgs.msg import Pose2D
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,6 +12,9 @@ print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 # Listen for incoming connections
 sock.listen(1)
+pub = rospy.Publisher('/nav_goal', Pose2D, queue_size=10)
+rospy.init_node('goal_publisher', anonymous=True)
+rate = rospy.Rate(100) # 10hz
 
 """
 starting up on 10.103.118.91 port 8000
@@ -22,6 +27,13 @@ no more data from ('10.103.95.125', 43562)
 waiting for a connection
 """
 
+named_locations = {"door" : [-1, -1, 0], "water": [-2, -7, 0], "middle": [-1.5, -3, 0]}
+def get_position_from_name(name):
+    try:
+        return named_locations[name.strip().lower()]
+    except:
+        return None
+
 while True:
     # Wait for a connection
     print >>sys.stderr, 'waiting for a connection'
@@ -32,10 +44,13 @@ while True:
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(4000)
-            print >>sys.stderr, 'received "%s"' % data
             if data:
-                print >>sys.stderr, 'sending data back to the client'
-                connection.sendall(data)
+                name = data.split('"location": ')[1].replace("}", "").replace('"', '').strip()
+                print(name)
+                pos = get_position_from_name(name)
+                if pos is not None:
+                    for _ in range(10):
+                        pub.publish(*pos)
             else:
                 print >>sys.stderr, 'no more data from', client_address
                 break
