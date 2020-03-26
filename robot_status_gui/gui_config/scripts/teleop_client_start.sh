2db6284cd60fb8b52a80d697f794aae69b028647
Kyle Vedder <kyle.c.vedder@gmail.com>
2020-03-26 15:59:58 -0400

    Added connection info to teleop start.

--- robot_status_gui/gui_config/scripts/teleop_client_start.sh
+++ robot_status_gui/gui_config/scripts/teleop_client_start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-screen -S teleop_client -dm ./src/ServiceRobotControlStack/control_stack/src/teleop/client.py &
+screen -S teleop_client -dm ./src/ServiceRobotControlStack/control_stack/src/teleop/client.py robot0 192.168.0.102 --http_port 5000 &
