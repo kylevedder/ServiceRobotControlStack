e2870691e41267953da8787570920b502a1fed05
Kyle Vedder <kyle.c.vedder@gmail.com>
2020-06-18 21:03:03 -0400

    Fixed format-all to ignore cmake-build-debug

--- ci/format_all.sh
+++ ci/format_all.sh
@@ -5,8 +5,8 @@ source_files=$(find catkin_ws/src/control_stack/ -name \*.cpp)
 echo "Formatting Headers!"
 
 for file in $header_files; do
-    # echo "Formatting: $file"
-    if [[ "$file" != *"control_stack/lib"* ]]; then
+    if [[ "$file" != *"control_stack/lib"* && "$file" != *"control_stack/cmake-build-debug"* ]]; then
+        # echo "Formatting: $file"
         clang-format -i "$file"
     fi
 done
@@ -14,8 +14,8 @@ done
 echo "Formatting Source!"
 
 for file in $source_files; do
-    # echo "Formatting: $file"
-    if [[ "$file" != *"control_stack/lib"* ]]; then
+    if [[ "$file" != *"control_stack/lib"* && "$file" != *"control_stack/cmake-build-debug"* ]]; then
+        # echo "Formatting: $file"
         clang-format -i "$file"
     fi
 done
