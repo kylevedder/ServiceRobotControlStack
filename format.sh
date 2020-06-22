b7f13dc6a19961d918dc1fbfcac7055c06ed29fd
Kyle Vedder <kyle.c.vedder@gmail.com>
2020-06-22 18:26:07 -0400

    Fixed format script

--- format.sh
+++ format.sh
@@ -1,23 +1,3 @@
 #!/usr/bin/env bash
 
-source /opt/ros/$ROS_DISTRO/setup.bash
-pushd catkin_ws
-catkin_make
-popd
-
-pushd rosbuild_ws
-export BASE_ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH
-
-pushd robot_status_gui/robot_status_gui
-export ROS_PACKAGE_PATH=`pwd`:$BASE_ROS_PACKAGE_PATH
-make -j `nproc`
-popd
-
-pushd simulator/f1tenth_simulator
-export ROS_PACKAGE_PATH=`pwd`:$BASE_ROS_PACKAGE_PATH
-make -j `nproc`
-popd
-
-popd
-export ROS_PACKAGE_PATH=$BASE_ROS_PACKAGE_PATH
-
+./ci/format_all.sh
