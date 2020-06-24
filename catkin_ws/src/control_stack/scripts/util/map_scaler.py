19a93447ac0eb56d5ea5805d6600eed0aeee0aa8
Kyle Vedder <kyle.c.vedder@gmail.com>
2020-06-24 14:18:10 -0400

    Swapped Y axis

--- catkin_ws/src/control_stack/scripts/util/map_scaler.py
+++ catkin_ws/src/control_stack/scripts/util/map_scaler.py
@@ -17,7 +17,8 @@ in_file, out_file, old_scale, new_scale = get_args()
 in_file = open(in_file, 'r')
 out_file = open(out_file, 'w')
 for row in csv.reader(in_file):
-    row = [str(float(e) / old_scale * new_scale) for e in row]
+    row = [float(e) / old_scale * new_scale for e in row]
+    row = [str(e) for e in row]
     out_file.write(', '.join(row) + '\n')
 in_file.close()
 out_file.close()
