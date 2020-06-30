e4c01d3ba2b072f1fe16c54429e79bcac012e95d
Kyle Vedder <kyle.c.vedder@gmail.com>
2020-06-30 19:24:06 -0400

    Properly reset the laser position and fixed the TF to reflect the proper location

--- catkin_ws/src/control_stack/include/cs/main/state_machine.h
+++ catkin_ws/src/control_stack/include/cs/main/state_machine.h
@@ -220,8 +220,9 @@ class StateMachine {
 
   void PublishTransforms() {
     tf::Transform laser_transform = tf::Transform::getIdentity();
-    laser_transform.setOrigin(
-        {params::CONFIG_laser_offset.x(), params::CONFIG_laser_offset.y(), 0});
+    laser_transform.setOrigin({-params::CONFIG_laser_offset.x(),
+                               -params::CONFIG_laser_offset.y(),
+                               0});
     br_.sendTransform(tf::StampedTransform(laser_transform,
                                            ros::Time::now(),
                                            params::CONFIG_laser_tf_frame,
